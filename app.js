3f28d42168c8fd36e62ff6e9e0fa766e333ae0cb
Hồ Thanh Khang <hothanh2092002@gmail.com>
2023-03-14 14:27:45 +0700

    xin lỗi mn tớ nộp trễ bài

---
+++ app.js
@@ -0,0 +1,105 @@
+// Menu effect
+
+
+
+function PageHome(){
+     document.querySelector('.navbar-menu-item a.active').classList.remove('active');
+     document.querySelector('#choosehome a').classList.add('active')
+     document.getElementById('product-list').style.display = 'block'
+     document.getElementById('best-sale').style.display = 'block'
+     document.getElementById('flash-sale').style.display = 'block'
+     document.getElementById('male').style.display = 'block'
+     document.getElementById('female').style.display = 'block'
+     document.getElementById('sub-architecture').style.display = 'block'
+     document.getElementById('infomation').style.display = 'block'
+     document.getElementById('brand').style.display = 'block'
+     document.getElementById('hero-section').style.display = 'block'
+     document.getElementById('hero-section-mini').style.display = 'block'
+     document.getElementById('bill').style.display = 'none'
+     document.getElementById('PageSearch').style.display = 'none'
+     document.getElementById('PageContact').style.display = 'none'
+     document.getElementById('PageCheckout').style.display = 'none'
+     document.getElementById('search').style.display = 'none'
+
+
+}
+
+function Subarchitecture(){
+     document.getElementById('product-list').style.display = 'none'
+     document.getElementById('best-sale').style.display = 'none'
+     document.getElementById('flash-sale').style.display = 'none'
+     document.getElementById('male').style.display = 'none'
+     document.getElementById('female').style.display = 'none'
+     document.getElementById('sub-architecture').style.display = 'block'
+     document.getElementById('infomation').style.display = 'none'
+     document.getElementById('brand').style.display = 'none'
+     document.getElementById('hero-section').style.display = 'none'
+     document.getElementById('hero-section-mini').style.display = 'none'
+     document.getElementById('bill').style.display = 'none'
+     document.getElementById('PageSearch').style.display = 'none'
+     document.getElementById('PageContact').style.display = 'none'
+     document.getElementById('PageCheckout').style.display = 'none'
+     document.getElementById('search').style.display = 'none'
+     document.getElementById('sub-architecture').scrollIntoView()
+
+}
+
+
+
+function PageBlog(){
+     document.querySelector('.navbar-menu-item a.active').classList.remove('active');
+     document.querySelector('#chooseinfo a').classList.add('active')
+     document.getElementById('product-list').style.display = 'none'
+     document.getElementById('best-sale').style.display = 'none'
+     document.getElementById('flash-sale').style.display = 'none'
+     document.getElementById('male').style.display = 'none'
+     document.getElementById('female').style.display = 'none'
+     document.getElementById('sub-architecture').style.display = 'none'
+     document.getElementById('infomation').style.display = 'none'
+     document.getElementById('brand').style.display = 'none'
+     document.getElementById('hero-section').style.display = 'none'
+     document.getElementById('hero-section-mini').style.display = 'none'
+     document.getElementById('PageContact').style.display = 'none'
+     document.getElementById('PageSearch').style.display = 'block'
+     document.getElementById('PageContact').style.display = 'none'
+     document.getElementById('PageCheckout').style.display = 'none'
+     document.getElementById('search').style.display = 'none'
+     document.getElementById('PageSearch').scrollIntoView()
+}
+
+function PageAbout() {
+     document.querySelector('.navbar-menu-item a.active').classList.remove('active');
+     document.querySelector('#choosecontact a').classList.add('active')
+     document.getElementById('product-list').style.display = 'none'
+     document.getElementById('best-sale').style.display = 'none'
+     document.getElementById('flash-sale').style.display = 'none'
+     document.getElementById('male').style.display = 'none'
+     document.getElementById('female').style.display = 'none'
+     document.getElementById('sub-architecture').style.display = 'none'
+     document.getElementById('infomation').style.display = 'none'
+     document.getElementById('brand').style.display = 'none'
+     document.getElementById('hero-section').style.display = 'none'
+     document.getElementById('hero-section-mini').style.display = 'none'
+     document.getElementById('PageSearch').style.display = 'none'
+     document.getElementById('PageCheckout').style.display = 'none'
+     document.getElementById('PageContact').style.display = 'block'
+     document.getElementById('search').style.display = 'none'
+     document.getElementById('PageContact').scrollIntoView()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
